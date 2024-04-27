6e19aada1b1c2c1bed13788f20e6b2cd4878a82a
Fahad Javed <Fahad_Javed008@yahoo.com>
2024-04-27 22:41:18 +0500

    ✅ Implemented the basic file server assignment from week 2

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -13,9 +13,55 @@
     Testing the server - run `npm run test-fileServer` command in terminal
  */
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const { readdir, readFile } = require('node:fs/promises');
+const { join, resolve } = require('node:path');
 const app = express();
 
+const SERVER_PORT = 8000;
+const FILES_DIRECTORY_PATH = resolve('files');
+
+// Create a dedicated router object to define the application routes so that these routes can later be moved to a separate file for a lean code structure
+const router = express.Router();
+
+// GET /files - Returns a list of files present in `./files/` directory
+router.get('/files', async (_, res) => {
+    try {
+        const files = await readdir(FILES_DIRECTORY_PATH);
+        return res.status(200).json(files);
+    } catch {
+        // some internal server error occurred
+        return res.status(500).send('Something went wrong. Please try again later.');
+    }
+});
+
+// GET /file/:filename - Returns content of given file by name
+router.get('/file/:filename', async (req, res) => {
+    const { filename } = req.params;
+
+    // construct the file path based on given filename and the underlying server's OS
+    const filePath = join(FILES_DIRECTORY_PATH, filename);
+
+    // read the file contents as a string
+    try {
+        const fileContents = await readFile(filePath, { encoding: 'utf-8' });
+        res.status(200).send(fileContents);
+    } catch {
+        // if an error is thrown, it means that the requested file does not exist on the server. Hence, return a "404 Not Found" response
+        res.status(404).send('File not found');
+    }
+});
+
+// any other route not defined above
+router.all('*', (_, res) => {
+    res.status(404).send('Route not found');
+});
+
+// Mount the router with app
+app.use('/', router);
+
+// Start the HTTP server
+app.listen(SERVER_PORT, () => {
+    console.log(`Server started listening on port ${SERVER_PORT}`);
+});
 
 module.exports = app;
