9cbf705f8c08d0e06e20913b958c255111588907
Fahad Javed <Fahad_Javed008@yahoo.com>
2024-04-30 03:42:54 +0500

    ✅ Implemented the error count global catch middleware for the express server

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
+
+const SERVER_PORT = 8000;
 
 const app = express();
 let errorCount = 0;
@@ -10,17 +10,33 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-app.get('/user', function(req, res) {
-  throw new Error("User not found");
-  res.status(200).json({ name: 'john' });
+app.get('/user', function (req, res) {
+    throw new Error('User not found');
+    res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post('/user', function (req, res) {
+    res.status(200).json({ msg: 'created dummy user' });
 });
 
-app.get('/errorCount', function(req, res) {
-  res.status(200).json({ errorCount });
+app.get('/errorCount', function (req, res) {
+    res.status(200).json({ errorCount });
+});
+
+// define the global catch middleware after all the routes have been registered in the app
+app.use((err, req, res, next) => {
+    // if `err` is not null, then we need to send the 404 response and increment the errorCount
+    if (err instanceof Error) {
+        errorCount += 1;
+        return res.status(404).send('Requested resource is not found');
+    }
+
+    // else, return the normal response to the user
+    next();
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server started listening on port ${SERVER_PORT}`);
 });
 
 module.exports = app;
