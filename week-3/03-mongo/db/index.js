7ac5ef3455bf9186a999aea61b0012d692c56673
Fahad Javed <Fahad_Javed008@yahoo.com>
2024-05-15 00:46:40 +0500

    Defined the MongoDB schemas for Admin, User, and Course collections
    Also setup dotenv file to manage the database connection string

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,50 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    purchasedCourses: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Course',
+        },
+    ],
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    published: Boolean,
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Admin',
+    },
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
@@ -23,5 +54,5 @@ const Course = mongoose.model('Course', CourseSchema);
 module.exports = {
     Admin,
     User,
-    Course
-}
+    Course,
+};
