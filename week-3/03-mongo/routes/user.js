b0830cc44807b0a514550f48c1ad18c705f32791
Fahad Javed <Fahad_Javed008@yahoo.com>
2024-05-15 02:33:56 +0500

    chore: Update user signup route to use zod for request body validation, and save the user account in database

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,10 +1,39 @@
-const { Router } = require("express");
+const { z } = require('zod');
+const { Router } = require('express');
 const router = Router();
-const userMiddleware = require("../middleware/user");
+const { User } = require('../db');
+
+const userMiddleware = require('../middleware/user');
+
+const userSignUpBodySchema = z.object({
+    username: z.string(),
+    password: z.string(),
+});
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const parsedBody = userSignUpBodySchema.safeParse(req.body);
+    if (!parsedBody.success) {
+        return res.status(400).json(parsedBody.error);
+    }
+
+    // request body is correct. Proceed with the rest of the validations
+    const { username, password } = parsedBody.data;
+
+    // Now we need to check if a user already exists with the given username or not
+    const userAlreadyExists = await User.findOne({ username });
+    if (userAlreadyExists) {
+        // an user with the given username already exists on the server
+        // return a "409 Conflict" response to let the user know
+        return res.status(409).json({ message: 'A user with the same username already exists. Please try again with a different username.' });
+    }
+
+    // user does not exist. Create the new user in the database
+    await User.create({ username, password });
+
+    // user account created. Send the success response
+    return res.status(200).json({ message: 'User created successfully' });
 });
 
 router.get('/courses', (req, res) => {
@@ -19,4 +48,4 @@ router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
 });
 
-module.exports = router
+module.exports = router;
