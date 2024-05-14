a301d03021c9a6b11a4274e8b1733b73d5739cb2
Fahad Javed <Fahad_Javed008@yahoo.com>
2024-05-15 00:48:51 +0500

    Implemented the Admin Signup route

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,10 +1,39 @@
-const { Router } = require("express");
-const adminMiddleware = require("../middleware/admin");
+const { z } = require('zod');
+const { Router } = require('express');
+const adminMiddleware = require('../middleware/admin');
+const { Admin } = require('../db');
+
 const router = Router();
 
+const signupBodySchema = z.object({
+    username: z.string(),
+    password: z.string(),
+});
+
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const parsedBody = signupBodySchema.safeParse(req.body);
+    if (!parsedBody.success) {
+        return res.status(400).json(parsedBody.error);
+    }
+
+    // request body is correct. Proceed with rest of the validation
+    const { username, password } = parsedBody.data;
+
+    // check if a user already exists with the given username or not
+    const adminAlreadyExists = await Admin.findOne({ username });
+    if (adminAlreadyExists) {
+        // an admin with the given username already exists on the server
+        // return a "409 Conflict" response to let the user know
+        return res.status(409).json({ message: 'An admin with the given username already exists' });
+    }
+
+    // admin with same username does not exist. Create the admin account
+    await Admin.create({ username, password });
+
+    // admin is created. Return the success response
+    return res.status(200).json({ message: 'Admin created successfully' });
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
