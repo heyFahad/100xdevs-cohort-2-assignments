91ef652b0698c9a17824893f778dce76e4b80df2
Fahad Javed <Fahad_Javed008@yahoo.com>
2024-05-16 08:39:58 +0500

    Designed the MongoDB schemas for the course selling app 2.0, and also configured the .env file for this project

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,21 +1,90 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
 // Define schemas
-const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-});
+const AdminSchema = new mongoose.Schema(
+    {
+        // Schema definition here
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
-const UserSchema = new mongoose.Schema({
-    // Schema definition here
-});
+const UserSchema = new mongoose.Schema(
+    {
+        // Schema definition here
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        purchasedCourses: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: 'Course',
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
 
-const CourseSchema = new mongoose.Schema({
-    // Schema definition here
-});
+const CourseSchema = new mongoose.Schema(
+    {
+        // Schema definition here
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: false,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        imageLink: {
+            type: String,
+            required: true,
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Admin',
+            required: true,
+        },
+        published: {
+            type: Boolean,
+            required: true,
+            default: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
+/**
+ * Generate the database models based on the schema definitions defined above
+ */
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
 const Course = mongoose.model('Course', CourseSchema);
@@ -23,5 +92,5 @@ const Course = mongoose.model('Course', CourseSchema);
 module.exports = {
     Admin,
     User,
-    Course
-}
+    Course,
+};
--- week-3/04-mongo-with-jwt-auth/index.js
+++ week-3/04-mongo-with-jwt-auth/index.js
@@ -1,13 +1,16 @@
+// load the environment variables in process.env as soon as the program starts
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const adminRouter = require("./routes/admin")
-const userRouter = require("./routes/user");
+const adminRouter = require('./routes/admin');
+const userRouter = require('./routes/user');
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
+app.use('/admin', adminRouter);
+app.use('/user', userRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
