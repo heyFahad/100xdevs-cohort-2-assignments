726432d56e4f06734535cde62a89545ab98d49cb
Fahad Javed <Fahad_Javed008@yahoo.com>
2024-05-19 02:29:02 +0500

    Implemented the admin middleware for the course selling app 2.0
    Also added a global catch middleware for catching all the server errors and sending the correct information to the user

--- week-3/04-mongo-with-jwt-auth/index.js
+++ week-3/04-mongo-with-jwt-auth/index.js
@@ -12,6 +12,16 @@ app.use(bodyParser.json());
 app.use('/admin', adminRouter);
 app.use('/user', userRouter);
 
+// Register a global catches middleware to handle all the errors originating from the server
+app.use((error, req, res, next) => {
+    res.status(error.status ?? 500).json({
+        message: error.message ?? 'Internal Server Error',
+    });
+
+    // Pass the error object to the next error handling middleware in the chain (maybe some error logging service)
+    next(error);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,45 @@
+const { promisify } = require('node:util');
+const { z } = require('zod');
+const { verify } = require('jsonwebtoken');
+
+const verifyAsync = promisify(verify);
+
+const authHeaderSchema = z.string().refine((value) => value.startsWith('Bearer '));
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const { authorization } = req.headers;
+    if (!authorization) {
+        return res.status(401).json({ message: 'Authorization header is required to proceed on this API' });
+    }
+
+    // First thing first. Check if the authorization header is in correct format or not
+    const parsedAuthorization = authHeaderSchema.safeParse(authorization);
+    if (!parsedAuthorization.success) {
+        return res.status(400).json({ message: "Authorization header improperly formatted. Expected format: 'Authorization: Bearer <token>'" });
+    }
+
+    // Authorization header is in correct format. Extract the access token from it
+    const authHeader = parsedAuthorization.data;
+    const token = authHeader.split(' ')[1]; // split the auth header to extract the JWT token from it
+
+    // verify this access token to see if it is a valid token or not
+    try {
+        const decodedToken = await verifyAsync(token, process.env.JWT_SECRET);
+
+        // token has been successfully decoded. Add this user's identity to the incoming request object and proceed
+        req.user = decodedToken;
+        next();
+    } catch (error) {
+        // log the error to some logging service
+        console.error({ error });
+
+        // return the error response
+        error.status = 401;
+        error.message = 'Invalid access token provided';
+        return next(error); // this error will be caught in the global catches middleware
+    }
 }
 
 module.exports = adminMiddleware;
