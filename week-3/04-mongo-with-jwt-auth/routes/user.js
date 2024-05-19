5f01a42823c92085b86dd1093dc0a9380d5e7eb7
Fahad Javed <Fahad_Javed008@yahoo.com>
2024-05-19 15:56:59 +0500

    feat: Implement user signup and signin routes with authentication and error handling in user module
    This commit adds the user signup and signin routes to the user module. It includes authentication logic using JWT tokens and proper error handling for invalid requests. The routes handle parsing and validation of the request body using Zod schemas. User passwords are securely hashed before storing them in the database. Successful signup returns a 201 status code with a success message, while signin returns a JWT access token for future authentication. Error responses are returned with appropriate status codes and error messages.

--- week-3/04-mongo-with-jwt-auth/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,14 +1,82 @@
-const { Router } = require("express");
-const router = Router();
-const userMiddleware = require("../middleware/user");
+const { promisify } = require('node:util');
+const { z } = require('zod');
+const { compare, hash } = require('bcrypt');
+const { Router } = require('express');
+const { sign } = require('jsonwebtoken');
+const { User } = require('../db');
+const userMiddleware = require('../middleware/user');
 
-// User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+// promisify the jwt.sign function to use it with async/await rather then using it with the callback style
+const signAsync = promisify(sign);
+
+// define the Zod schemas to parse the incoming requests' body
+const userAuthBodySchema = z.object({
+    username: z.string().min(3),
+    password: z.string().min(6),
 });
 
-router.post('/signin', (req, res) => {
+// create the Router object and define routes for this module
+const router = Router();
+
+// User Routes
+router.post('/signup', async (req, res) => {
+    // Implement user signup logic
+    const parsedBody = userAuthBodySchema.safeParse(req.body);
+    if (!parsedBody.success) {
+        return res.status(400).json(parsedBody.error);
+    }
+
+    // request payload is correct. Proceed with the next validations.
+    const { username, password: rawPassword } = parsedBody.data;
+
+    // Check if a user already exists with the given username or not
+    const userAlreadyExists = await User.findOne({ username });
+    if (userAlreadyExists) {
+        return res.status(409).json({ message: 'A user already exist with this username' });
+    }
+
+    // If no user exists with this username, we then need to hash the incoming user's password before storing it in the database
+    const saltRounds = 10;
+    const hashedPassword = await hash(rawPassword, saltRounds);
+
+    // now the data is prepared. Let's save this user into the database
+    await User.create({
+        username,
+        password: hashedPassword,
+    });
+
+    // finally send the success response to the user
+    return res.status(201).json({ message: 'User created successfully' });
+});
+
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+    const parsedBody = userAuthBodySchema.safeParse(req.body);
+    if (!parsedBody.success) {
+        return res.status(400).json(parsedBody.error);
+    }
+
+    // request body is correct. Proceed with the next validations
+    const { username, password } = parsedBody.data;
+
+    // check if any user exists with the given username or not
+    const userExists = await User.findOne({ username });
+    if (!userExists) {
+        return res.status(404).json({ message: 'User does not exist. Please signup for a new account' });
+    }
+
+    // if the user exists, we need to check if the incoming request has correct user credentials or not
+    const isPasswordMatched = await compare(password, userExists.password);
+    if (!isPasswordMatched) {
+        return res.status(403).json({ message: 'Username or password is incorrect' });
+    }
+
+    // user authentication is done now. User is valid and their credentials are also correct
+    // We now need to create a JWT access token for this user so that they can authenticate their future requests
+    const token = await signAsync(userExists.toObject(), process.env.JWT_SECRET);
+
+    // return this token to the user for future requests
+    return res.status(200).json({ token });
 });
 
 router.get('/courses', (req, res) => {
@@ -23,4 +91,4 @@ router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
 });
 
-module.exports = router
+module.exports = router;
